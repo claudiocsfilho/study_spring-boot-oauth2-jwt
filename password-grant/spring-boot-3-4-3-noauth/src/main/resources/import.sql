1aed767a455c60e920257bc6d29bdef689002a7e
Claudio Filho <claudiocsfilho@gmail.com>
2025-03-07 16:16:41 -0300

    Add BCrypt password

--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$o5ZFJ.8wQNi8hTqJVTfIgOeqOS6PQYaDS11qSLe9tulzb0Rc7FVle');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$o5ZFJ.8wQNi8hTqJVTfIgOeqOS6PQYaDS11qSLe9tulzb0Rc7FVle');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
